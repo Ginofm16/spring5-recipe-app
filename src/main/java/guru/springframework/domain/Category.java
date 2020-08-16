39d1f6b91c61af9b809db9a248c1c64778883439
gino <gino@gmail.com>
2020-08-15 22:10:08 -0500

    first commit: JPA Data modeling, Repository

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,40 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descripcion;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
